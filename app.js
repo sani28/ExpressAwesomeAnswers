f93c421549eb54927758b666c2b530b20b15b26b
sani28 <strinh@sfu.ca>
2017-08-01 09:40:10 -0700

    Tweaked base app Changed port to 3333 Made vars in const in app.js Switched from sass to scss(indentedSyntax:false) Git ignore *.css and *.css.map

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const  express = require('express');
+const  path = require('path');
+const  favicon = require('serve-favicon');
+const  logger = require('morgan');
+const  cookieParser = require('cookie-parser');
+const  bodyParser = require('body-parser');
+const  nodeSass = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const  index = require('./routes/index');
+vconst users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +21,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
+app.use(nodeSass({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
