a3134be4a251b58836e4025c9623ca13ad881a55
sani28 <strinh@sfu.ca>
2017-08-01 11:41:37 -0700

    added partials for header and footer, did views on questions

--- routes/questions.js
+++ routes/questions.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const {Question} = require('../models');
 
+
+
+// questions#index PATH: /questions METHOD: GET
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   Question
-    .all()
-    .then(questions => {
-      res.send(questions);
+    .all() //return a promise
+    .then(questions => { //if we want to get a value from a promise we have to use THEN, so the questions here are the resolved values in an array for querying Questions 
+      //to pass a variable to a template, pass an
+      //object as a second argument to res.render.
+      //all the properites of that object will be available
+      //as local variables inside of the template.
+      res.render('questions/index', {questions});
     })
 });
 
